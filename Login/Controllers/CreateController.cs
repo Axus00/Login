a925346245861058a6d336d704c32b0be131d642
Fernando <fjgt2000@gmail.com>
2024-04-19 19:21:38 -0500

    Se realiza encriptamiento de password

--- Login/Controllers/CreateController.cs
+++ Login/Controllers/CreateController.cs
@@ -5,6 +5,7 @@ using Login.Providers;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
+using System;
 
 
 namespace Login.Controllers;
@@ -34,11 +35,14 @@ public class CreateController : Controller
     
 
     [HttpPost]
-    public async Task<IActionResult> Index(Employee employee, IFormFile archivo, int ubicacion)
+    public async Task<IActionResult> Index(Employee employee, IFormFile archivo, int ubicacion, string password)
     {
         string nombreArchivo = archivo.FileName;
         string path = "";
 
+        
+        //cifrado de contraseña
+        string passwordCifrado = CifrarPassword(password, 4);
         
         
         
@@ -59,9 +63,45 @@ public class CreateController : Controller
         }
 
         employee.Photo = nombreArchivo;
+        employee.Password = passwordCifrado;
         _context.Employees.Add(employee);
         await _context.SaveChangesAsync();
         ViewBag.Message = "Se ha creado de forma exitosa";
         return RedirectToAction("Index");
+    }
+
+    private string CifrarPassword(string password, int movimiento)
+    {
+        string letters = "abcdefghijklmnñopqrstuvwxyz";
+        string passwordCifrado = "";
+
+        foreach (char caracter in password)
+        {
+            if (char.IsLetter(caracter))
+            {
+                int posicionCaracter = letters.IndexOf(char.ToLower(caracter));
+                int nuevaPosicion = (posicionCaracter + movimiento) % letters.Length;
+                char otroCaracter = letters[nuevaPosicion];
+                passwordCifrado += char.IsUpper(caracter) ? char.ToUpper(otroCaracter) : otroCaracter;
+            }
+            else
+            {
+                passwordCifrado += caracter;
+            }
+
+            if (char.IsNumber(caracter))
+            {
+                int posicionCaracter = letters.IndexOf(char.ToLower(caracter));
+                int nuevaPosicion = (posicionCaracter + movimiento) % letters.Length;
+                char otroCaracter = letters[nuevaPosicion];
+                passwordCifrado += char.IsUpper(caracter) ? char.ToUpper(otroCaracter) : otroCaracter;
+            }
+            else
+            {
+                passwordCifrado += caracter;
+            }
+        }
+
+        return passwordCifrado;
     }
 }
